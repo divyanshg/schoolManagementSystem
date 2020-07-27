5c67f4825d2b46e87b3f750370dc0fd65b6a320f
yuvraj192 <yuvrajverma192@gmail.com>
2020-07-27 23:40:33 +0530

    aTLAS srv

--- models/database.js
+++ models/database.js
@@ -2,23 +2,32 @@ require('dotenv').config()
 
 var mongodb = require('mongodb')
 var MongoClient = mongodb.MongoClient;
-var url = process.env.MONGO_URI;
+var url = 'mongodb+srv://yuvraj192:div21902@sms.pni8a.mongodb.net/SMS?retryWrites=true&w=majority';
 let connection = null;
 
 var dataCamp;
 
 module.exports.connect = () => new Promise((resolve, reject) => {
-    MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
-        if (err) { reject(err); return; };
-        
-        dataCamp = db.db(process.env.DATABASE);
-        resolve(db);
-        
-    });
+    try {
+        MongoClient.connect(url, {
+            useUnifiedTopology: true
+        }, function (err, db) {
+            if (err) {
+                reject(err);
+                return;
+            };
+
+            dataCamp = db.db(process.env.DATABASE);
+            resolve(db);
+
+        });
+    } catch (e) {
+        console.log(e)
+    }
 });
 
 module.exports.get = () => {
-    if(!dataCamp || typeof dataCamp == "undefined") {
+    if (!dataCamp || typeof dataCamp == "undefined") {
         throw new Error('Call connect first!');
     }
 
